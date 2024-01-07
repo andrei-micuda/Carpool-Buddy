ebe055534bf45aa48b708606bfecd5e8db408d06
Andrei Micuda <andrei.micuda@gmail.com>
2024-01-07 21:47:18 +0000

    Initial user geolocation implementation

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -1,25 +1,79 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import tw from "tailwind-styled-components";
 import mapboxgl from "!mapbox-gl";
 
-mapboxgl.accessToken = "YOUR_ACCESS_TOKEN";
+mapboxgl.accessToken = "NULL";
 
 const Map = (props) => {
+  console.log("Initializing map...");
+
+  const map = useRef(null);
+
+  const [coordinates, setCoordinates] = useState([-74.5 + (Math.random() - 0.5) * 10, // Example data
+  40 + (Math.random() - 0.5) * 10])
+
   useEffect(() => {
     const pickupCoord = props.pickupCoordinate;
     const dropoffCoord = props.dropoffCoordinate;
 
-    const map = new mapboxgl.Map({
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
       container: "map",
       style: "mapbox://styles/mapbox/streets-v11",
       center: [-96, 37.8],
       zoom: 3,
     });
 
-    if (pickupCoord && dropoffCoord) {
-      addToMap(map, pickupCoord, dropoffCoord);
+    var geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      // When active the map will receive updates to the device's location as it changes.
+      trackUserLocation: true,
+      // Draw an arrow next to the location dot to indicate which direction the device is heading.
+      showUserHeading: true
+      });
+    
+      geolocate.on('geolocate', (pos) => {
+        centerMap(map.current, [pos.coords.latitude, pos.coords.longitude]);
+        console.log('A geolocate event has occurred.');
+      });
+
+    map.current.addControl(
+      geolocate
+    );
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setCoordinates([position.coords.longitude, position.coords.latitude]);
+        // centerMap(map, coordinates);
+        // console.log(coordinates);
+      });
     }
-  });
+
+    map.current.on('load', function()
+    {
+      geolocate.trigger();
+    });
+
+    if (pickupCoord && dropoffCoord) {
+      addToMap(map.current, pickupCoord, dropoffCoord);
+    }
+  }, []);
+
+  useEffect(() => centerMap(map.current, coordinates), [coordinates]);
+
+  const centerMap = (map, coordinates) => {
+    map.on('load', function () {
+      map.flyTo({
+         center: [
+            coordinates[0],
+            coordinates[1]
+         ],
+         essential: true // this animation is considered essential with respect to prefers-reduced-motion
+      });
+    });
+  }
 
   const addToMap = (map, pickupCoord, dropoffCoord) => {
     // Create a default Marker and add it to the map.
